c28738bf737b03d961ae8044b8ada348e45022f4
sethcarrino <sethcarrino@gmail.com>
2017-08-03 19:42:27 -0400

    fixed the login form to include authentication - switch back to mustache

--- index.js
+++ index.js
@@ -1,103 +1,58 @@
+let username;
+let password;
+
 const express = require('express');
-const exphbs = require('express-handlebars');
+const mustache = require('mustache-express');
 const bodyParser = require('body-parser');
-const moment = require('moment');
-const morgan = require('morgan');
 const expressValidator = require('express-validator');
 const session = require('express-session');
-const users = require('./users.js')
-const fs = require('fs');
+
+const data = require('./users.js');
+
 const app = express();
 
-
-// use handlbars
-app.engine('handlebars', exphbs());
-app.set('views', './views');
-app.set('view engine', 'handlebars');
-
-// configure session middleware
-app.use(
-  session({
-    secret: 'V4P3N4710N',
-    resave: false,
-    saveUninitialized: true
-  })
-)
-
-// setup morgan to log requests
-app.use(morgan('dev'));
-
-// tell express how to serve static files
-app.use(express.static('public'));
-
-// use body parser to parse form data
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
-// use express validator
 app.use(expressValidator());
 
-// create default session
-app.use((req, res, next) => {
+app.use(session({
+  secret: 'V4P3N4710N',
+  resave: false,
+  saveUninitialized: true
+}));
 
-  if (!req.session.users) {
+app.engine('mustache', mustache());
+app.set('view engine', 'mustache');
+app.set('views', './views');
 
-    req.session.users = [];
-
+app.get('/', function(req, res){
+  if (req.session && req.session.authenticated){
+    // res.render('index');
+    res.send("You have successfully logged in under the username: " + username);
+  } else {
+    res.redirect('/login/');
   }
-  console.log(req.session);
-
-  next();
 });
 
+app.post('/', function(req, res){
+  res.redirect('/login/');
+});
 
-app.get('/', (req, res) => {
-  if (req.session.users === undefined || req.session.users.length == 0){
-    res.redirect('/login');
-  } else {
-    let users = req.session;
-    console.log(users);
-    res.render('home', {
-      users: users
-    })
-  }
+app.get('/login/', function(req, res){
+  res.render('login');
+});
 
+app.post('/login/', function(req, res){
+  username = req.body.username;
+  password = req.body.password;
 
-})
-
-app.get('/login', (req, res) =>{
-
-  res.render('login')
-})
-
-app.post('/login', (req, res) =>{
-  let userInfo = req.body;
-
-  req.checkBody('email', 'Email is Required').notEmpty();
-
-  req.checkBody('password', 'Password is Required').notEmpty();
-
-  let errors = req.validationErrors();
-
-  if  (errors) {
-    console.log(errors);
-
-    res.render('login', {
-      errors: errors,
-      userInfo: userInfo
-
-    })
-
-  } else {
-
-    req.session.users.push(userInfo);
-    users.push(userInfo);
-    console.log(users);
-
-
+  if (data.authenticate(req, username, password)){
     res.redirect('/');
+  } else{
+    res.render('home');
   }
-})
+});
 
-// listen on port 3000
-app.listen(3000);
+app.listen(3000, function () {
+  console.log('Successfully started express application!');
+});
